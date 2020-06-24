447bcb7f3693b34e6bb82fdd844147f6567ae833
ShubhamSharma90113 <47415702+ShubhamSharma90113@users.noreply.github.com>
2020-06-24 17:06:33 +0530

    Update Graph_Traversal.cpp

--- Graph_Traversal.cpp
+++ Graph_Traversal.cpp
@@ -100,21 +100,129 @@ void TopoSort(Graph* G,int s){
     }
 }
 
+/*void DFStimerRec(Graph* G,bool reach[],int x,int intime[],int outtime[],int timer){
+    reach[x] = true;
+    ++timer;
+    intime[x] = timer;
+    auto it = G->adj[x].begin();
+    while(it != G->adj[x].end()){
+        if(!reach[*it]){
+            DFStimerRec(G,reach,*it,intime,outtime,timer);
+        }
+    }
+    ++timer;
+    outtime[x] = timer;
+}
+
+void DFStimer(Graph* G,int x){
+    int timer= 0;
+    bool* reach = new bool[G->V];
+    int* intime = new int[G->V];
+    int* outtime = new int[G->V];
+    for(int i=0;i<G->V;i++){
+        reach[i] = false;
+        intime[i] = 0;
+        outtime[i] = 0;
+    }    
+    DFStimerRec(G,reach,x,intime,outtime,timer);
+    for(int i=0;i<G->V;i++){
+        if(!reach[i]){
+            DFStimerRec(G,reach,i,intime,outtime,timer);
+        }
+    }
+    for(int i=0;i<G->V;i++){
+        cout<<intime[i]<<" "<<outtime[i]<<endl;
+    }
+    //return (((intime[u] < intime[v]) && (outtime[u] > outtime[v])) || ((intime[u] > intime[v]) && (outtime[u] < outtime[v])));
+}*/
+
+bool isReachable(Graph* G,int x,int y){
+    bool* reached = new bool[G->V];
+    for(int i=0;i<G->V;i++){
+        reached[i] = false;
+    }
+    if (x==y) return true;
+    list<int> Qu;
+    Qu.push_back(x);
+    reached[x] = true;
+    while(!Qu.empty()){
+        x = Qu.front();
+        Qu.pop_front();
+        for(auto it = G->adj[x].begin(); it != G->adj[x].end(); ++it){
+            if(*it == y){
+                return true;
+            }
+            else{
+                if(!reached[*it]){
+                    reached[*it] = true;
+                    Qu.push_back(*it);
+                }
+            }
+        }
+    }
+    return false;
+}
+
+bool isCycleRec(Graph* G,int x,bool place[],bool stack[]){
+    if (place[x] == false){
+        place[x] = true;
+        stack[x] = true;
+    }
+    for(auto it = G->adj[x].begin();it != G->adj[x].end();++it){
+        if(!place[*it] && isCycleRec(G,*it,place,stack)){
+            return true;
+        }
+        else if(stack[*it]){
+            return true;
+        }
+    }
+    stack[x] = false;
+    return false;
+}
+
+bool isCycle(Graph* G){
+    bool* place = new bool[G->V];
+    bool* stack = new bool[G->V];
+    for(int i=0;i<G->V;i++){
+        place[i] = false;
+        stack[i] = false;
+    }
+    for(int i=0;i<G->V;i++){
+        if (isCycleRec(G,i,place,stack)){
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
 	Graph* G;
-	G = Adjlist(6);
-	addEdge(G,5,0);
+	G = Adjlist(4);
+	/*addEdge(G,5,0);
 	addEdge(G,4,0);
 	addEdge(G,5,2);
 	addEdge(G,2,3);
 	addEdge(G,3,1);
 	addEdge(G,4,1);
+	*/
+	addEdge(G,0,1);
+	addEdge(G,1,2);
+	//addEdge(G,2,0);
+	addEdge(G,0,2);
+	addEdge(G,2,3);
 	Print(G);
 	cout<<endl;
-	BFS(G,5);
+	BFS(G,0);
 	cout<<endl;
-	DFS(G,5);
+	DFS(G,0);
 	cout<<endl;
-	TopoSort(G,5);
+	TopoSort(G,0);
+	cout<<endl;
+    //(DFStimer(G,5));
+    if(isReachable(G,0,3)) cout<<"YES";
+    else cout<<"NO";
+    cout<<endl;
+    if(isCycle(G)) cout<<"Yes";
+    else cout<<"NO";
 	return 0;
 }
